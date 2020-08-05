8f8254800fdd352ebc3be611051707e6a60d83b2
Caio-Evangelista <cfevangelista06@gmail.com>
2020-08-05 09:53:51 -0300

    add room feature

--- scripts/draw.js
+++ scripts/draw.js
@@ -4,108 +4,117 @@ var cw = canvas.width;
 var ch = canvas.height;
 
 var binW = 45;
-var recW = (binW/1000)*cw;
-var recW2 = 3*binW;
-var recW3 = 5*binW;
+var recW = (binW / 1000) * cw;
+var recW2 = 3 * binW;
+var recW3 = 5 * binW;
 
 
 
-function position(x){
-		return 5 + (x/1005)*cw;
+function position(x) {
+	return 5 + (x / 1005) * cw;
 }
 
-function draw(x){
-		posx = position(x)
-		ctx.beginPath();
-		ctx.fillStyle = "#d3961f";
-		ctx.fillRect(posx-recW3/2, 0, recW3, 150);
-		ctx.stroke();
+function draw(x) {
+	posx = position(x)
+	ctx.beginPath();
+	ctx.fillStyle = "#d3961f";
+	ctx.fillRect(posx - recW3 / 2, 0, recW3, 150);
+	ctx.stroke();
 
-		ctx.beginPath();
-		ctx.fillStyle = "#a4cea3";
-		ctx.fillRect(posx-recW2/2, 0, recW2, 150);
-		ctx.stroke();
+	ctx.beginPath();
+	ctx.fillStyle = "#a4cea3";
+	ctx.fillRect(posx - recW2 / 2, 0, recW2, 150);
+	ctx.stroke();
 
-		ctx.beginPath();
-		ctx.fillStyle = "#dd5d3e";
-		ctx.fillRect(posx-recW/2, 0, recW, 150);
-		ctx.stroke();
+	ctx.beginPath();
+	ctx.fillStyle = "#dd5d3e";
+	ctx.fillRect(posx - recW / 2, 0, recW, 150);
+	ctx.stroke();
 
 }
 
-function drawguess(){
-		clearboard()	
-		draw(randpos)
+function drawguess() {
+	clearboard()
+	draw(randpos)
 
-		var guess = document.getElementById("guesser").value
-		ctx.beginPath();
-		ctx.fillStyle = "#d42838";
-		ctx.fillRect(position(guess)-2, 0, 4, 150);
-		ctx.stroke();
+	var guess = document.getElementById("guesser").value
+	ctx.beginPath();
+	ctx.fillStyle = "#d42838";
+	ctx.fillRect(position(guess) - 2, 0, 4, 150);
+	ctx.stroke();
 
-		score(randpos, guess)
+	score(randpos, guess)
 
 
 }
 
 var points = 0
 
-function score(randpos, guess){
-		if (between(guess, randpos - binW/2, randpos + binW/2)){
-				document.getElementById("score").innerHTML = '<div class="score">4 points!!!</div>';
-				points = 4;
-		} else if (between(guess, randpos - 3*binW/2, randpos + 3*binW/2)){
-				document.getElementById("score").innerHTML = '<div class="score">3 points!!</div>';
-				points = 3;
-		} else if (between(guess, randpos - 5*binW/2, randpos + 5*binW/2)){
-				document.getElementById("score").innerHTML = '<div class="score">2 points!</div>';
-				points = 2;
-		} else {
-				document.getElementById("score").innerHTML = '<div class="score">0 points</div>';
-				points = 0;
-		}
+function score(randpos, guess) {
+	if (between(guess, randpos - binW / 2, randpos + binW / 2)) {
+		document.getElementById("score").innerHTML = '<div class="score">4 points!!!</div>';
+		points = 4;
+	} else if (between(guess, randpos - 3 * binW / 2, randpos + 3 * binW / 2)) {
+		document.getElementById("score").innerHTML = '<div class="score">3 points!!</div>';
+		points = 3;
+	} else if (between(guess, randpos - 5 * binW / 2, randpos + 5 * binW / 2)) {
+		document.getElementById("score").innerHTML = '<div class="score">2 points!</div>';
+		points = 2;
+	} else {
+		document.getElementById("score").innerHTML = '<div class="score">0 points</div>';
+		points = 0;
+	}
 }
 
 
 function between(x, min, max) {
-		return x >= min && x <= max;
+	return x >= min && x <= max;
 }
 
-function button_peek(){
-		if (window.confirm("Are you sure you want to peek?")) {
-				draw(randpos);
-		}
-		gtag('event', 'peek');
+function button_peek() {
+	if (window.confirm("Are you sure you want to peek?")) {
+		draw(randpos);
+	}
+	gtag('event', 'peek');
 }
 
 
-function button_guess(){
-		if (window.confirm("Is this your final guess?")) {
-				drawguess();
-		}
-		gtag('event', 'guess');
+function button_guess() {
+	if (window.confirm("Is this your final guess?")) {
+		drawguess();
+	}
+	gtag('event', 'guess');
 }
 
-function update_seed(){
-		Math.seedrandom();
+function update_seed() {
+	Math.seedrandom();
+	var seed = document.getElementById("seed").value;
+	var room = document.getElementById("room").value
+	var nextSeed = Math.floor(parseInt(seed) + parseInt(room, 36) % 1000 + 1);
+
+	if (!isNaN(seed) && !isNaN(nextSeed)) {
+		$("#seed").val(nextSeed);
+	} else {
 		$("#seed").val(Math.floor(Math.random() * 10000));
-		fire();
-		gtag('event', 'new_clue');
+		$("#room").val(Math.floor(Math.random() * 10000));
+	}
+	fire();
+	gtag('event', 'new_clue');
 }
 
 
-function update_percentages(){
-		var checkbox = document.getElementById("percentages");
-		var text = document.getElementById("guessdisp");
-		if (checkbox.checked == true){
-				text.style.display = "block";
-		} else {
-				text.style.display = "none";
-		}
-		gtag('event', 'display_percentage');
+function update_percentages() {
+	var checkbox = document.getElementById("percentages");
+	var text = document.getElementById("guessdisp");
+	if (checkbox.checked == true) {
+		text.style.display = "block";
+	} else {
+		text.style.display = "none";
+	}
+	gtag('event', 'display_percentage');
 }
 
-function button_clear(){
-		clearboard();
-		gtag('event', 'clear_board');
+function button_clear() {
+	clearboard();
+	gtag('event', 'clear_board');
 }
